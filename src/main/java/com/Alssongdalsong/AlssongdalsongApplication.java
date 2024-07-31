6905a564ab858ed9936d8a8bac9dd5f8ce03264b
JUNG-EUN1997 <rro0628@gmail.com>
2024-07-31 11:20:47 +0900

    spring setting

---
+++ src/main/java/com/Alssongdalsong/AlssongdalsongApplication.java
@@ -0,0 +1,13 @@
+package com.Alssongdalsong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlssongdalsongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlssongdalsongApplication.class, args);
+	}
+
+}
