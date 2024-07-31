3d38235feb65b642aa80c231d1690e4c61215ceb
JUNG-EUN1997 <rro0628@gmail.com>
2024-07-31 12:10:33 +0900

    feat: common 기능 세팅

---
+++ src/main/java/com/Alssongdalsong/common/dto/CommonErrorDto.java
@@ -0,0 +1,14 @@
+package com.Alssongdalsong.common.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommonErrorDto {
+    private int status_code;
+    private String error_message;
+}
